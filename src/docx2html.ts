ebd3f2229a7d40ed1b8c2754371efd00b299079e
李众豪 <lizhonghao@fsyhlz.com>
2023-02-14 22:47:53 +0800

    docs

--- src/docx2html.ts
+++ src/docx2html.ts
@@ -2,7 +2,7 @@ import { renderAsync } from 'docx-preview'
 
 export default async function docx2html(file: File) {
   const container = document.createElement('div')
-  await renderAsync(file, container, void 0, { inWrapper: false })
+  await renderAsync(file, container, void 0, { inWrapper: false, useBase64URL: true })
 
   for (const el of container.querySelectorAll('*')) {
     for (let child = el.firstChild; (child = child?.nextSibling); ) {
@@ -18,5 +18,5 @@ export default async function docx2html(file: File) {
 
 export const isTextNode = (node: ChildNode) => node.nodeType == 3
 export const isSpanText = (node?: Element) => node?.tagName === 'SPAN' && node.childNodes.length == 1 && isTextNode(node.firstChild!)
-export const isSomeAttributes = (e1?: Element, e2?: Element) => e1?.attributes.length == e2?.attributes.length && e1?.getAttributeNames().every(k => e1?.getAttribute(k) == e2?.getAttribute(k))
-export const isMergeable = (e1?: Node, e2?: Node) => isSpanText(e1 as Element) && isSpanText(e2 as Element) && isSomeAttributes(e1 as Element, e2 as Element)
+export const isSameAttributes = (e1?: Element, e2?: Element) => e1?.attributes.length == e2?.attributes.length && e1?.getAttributeNames().every(k => e1?.getAttribute(k) == e2?.getAttribute(k))
+export const isMergeable = (e1?: Node, e2?: Node) => [e1, e2].every(isSpanText) && isSameAttributes(e1 as Element, e2 as Element)
